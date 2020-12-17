fbf1b569ec6a99b7200c203104e9d8dd6ae096c3
Jason Reed <Jreed40@illinois.edu>
2020-12-16 22:32:15 -0600

    Adding init files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Calender from './calendar'
+
+const startTime = 300
+const endTime = 600
+const jump = 15
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Calender start={startTime} end={endTime} jump={jump} />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
